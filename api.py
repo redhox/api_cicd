29d39a2195fa854913cac2a7ee1188d7e9156962
redhox <morgan.c@neuf.fr>
2024-03-19 11:43:54 +0100

    auto install8

--- api.py
+++ api.py
@@ -18,14 +18,14 @@ tags = [
        },
        {
               "name": "Predict",
-              "description": "Prediction en cicd full auto",
+              "description": "Prediction en cicd",
        },
 ]
 
 # Création de l'application
 app = FastAPI(
        title="API de prediction en cicd",
-       description= "Predictions qui a pour but d'etre un test de production cicd full auto",
+       description= "Predictions qui a pour but d'etre un test de production cicd",
        version= "1.0.0",
        openapi_tags= tags
 )
